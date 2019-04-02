3c23c606b7715c19bbdc7bdd29f0969aa11878f3
you <you>
2019-04-03 00:23:21 +0800

    again and again

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ agda-mode setup
 agda-mode compile
 mkdir -p $INSTALLATION_PATH
 git clone https://github.com/agda/agda-stdlib.git $INSTALLATION_PATH/agda-stdlib/
-rm INSTALLATION_PATH/agda-stdlib/src/index.agda
+rm $INSTALLATION_PATH/agda-stdlib/src/index.agda
 mkdir ~/.agda
 echo "$INSTALLATION_PATH/agda-stdlib/standard-library.agda-lib" >> ~/.agda/libraries
 echo "standard-library" >> ~/.agda/defaults
