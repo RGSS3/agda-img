4639abf369d1d85052eafd4ad86a7485496e1a89
you <you>
2019-04-16 14:40:45 +0800

    again

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ cabal install alex happy cpphs
 cabal install --allow-newer Agda
 agda-mode setup
 agda-mode compile
-mkdir -p INSTALLATION_PATH
-git clone https://github.com/agda/agda-stdlib.git INSTALLATION_PATH/agda-stdlib/
+mkdir -p $INSTALLATION_PATH
+git clone https://github.com/agda/agda-stdlib.git $INSTALLATION_PATH/agda-stdlib/
 rm INSTALLATION_PATH/agda-stdlib/src/index.agda
 mkdir ~/.agda
-echo "INSTALLATION_PATH/agda-stdlib/standard-library.agda-lib" >> ~/.agda/libraries
+echo "$INSTALLATION_PATH/agda-stdlib/standard-library.agda-lib" >> ~/.agda/libraries
 echo "standard-library" >> ~/.agda/defaults
